442c29fac3c538d30735e8973b19953c78c0a02b
ShaneTaylor-IT <s.a.taylor23@outlook.com>
2026-06-27 15:20:47 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAddtionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivisionQuestion() {
+    
+}
